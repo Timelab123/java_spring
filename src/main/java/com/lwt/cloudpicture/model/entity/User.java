cf1a7a56cd3aaa05e658e37cc8a2ec9145715625
18509 <1286547022@qq.com>
2025-04-29 09:44:50 +0800

    springboot

--- src/main/java/com/lwt/cloudpicture/model/entity/User.java
+++ src/main/java/com/lwt/cloudpicture/model/entity/User.java
@@ -1,7 +1,6 @@
-package generator.domain;
+package com.lwt.cloudpicture.model.entity;
 
 import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import java.util.Date;
--- src/main/java/generator/mapper/UserMapper.java
+++ src/main/java/generator/mapper/UserMapper.java
@@ -1,13 +1,13 @@
 package generator.mapper;
 
-import generator.domain.User;
+import com.lwt.cloudpicture.model.entity.User;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 
 /**
 * @author 18509
 * @description 针对表【user(用户)】的数据库操作Mapper
 * @createDate 2025-04-27 17:56:56
-* @Entity generator.domain.User
+* @Entity com.lwt.cloudpicture.model.entity.User
 */
 public interface UserMapper extends BaseMapper<User> {
 
